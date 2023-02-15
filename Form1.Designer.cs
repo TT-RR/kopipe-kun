dc382b711b25e01802615deeb1bb180026ec677f
yuzawa <busieasy98@gmail.com>
2023-02-15 16:10:21 +0900

    form1の表示名をコピペ君に変更

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,99 +46,110 @@ namespace kopipe_kun
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(5, 66);
+            this.button1.Location = new System.Drawing.Point(4, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(270, 50);
+            this.button1.Size = new System.Drawing.Size(216, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(5, 115);
+            this.button2.Location = new System.Drawing.Point(4, 96);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(270, 50);
+            this.button2.Size = new System.Drawing.Size(216, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(5, 164);
+            this.button3.Location = new System.Drawing.Point(4, 137);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(270, 50);
+            this.button3.Size = new System.Drawing.Size(216, 42);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(5, 213);
+            this.button4.Location = new System.Drawing.Point(4, 177);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(270, 50);
+            this.button4.Size = new System.Drawing.Size(216, 42);
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(5, 262);
+            this.button5.Location = new System.Drawing.Point(4, 218);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(270, 50);
+            this.button5.Size = new System.Drawing.Size(216, 42);
             this.button5.TabIndex = 4;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(5, 311);
+            this.button6.Location = new System.Drawing.Point(4, 259);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(270, 50);
+            this.button6.Size = new System.Drawing.Size(216, 42);
             this.button6.TabIndex = 5;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(5, 360);
+            this.button7.Location = new System.Drawing.Point(4, 300);
+            this.button7.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(270, 50);
+            this.button7.Size = new System.Drawing.Size(216, 42);
             this.button7.TabIndex = 6;
             this.button7.Text = "button7";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(5, 409);
+            this.button8.Location = new System.Drawing.Point(4, 341);
+            this.button8.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(270, 50);
+            this.button8.Size = new System.Drawing.Size(216, 42);
             this.button8.TabIndex = 7;
             this.button8.Text = "button8";
             this.button8.UseVisualStyleBackColor = true;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(5, 458);
+            this.button9.Location = new System.Drawing.Point(4, 382);
+            this.button9.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(270, 50);
+            this.button9.Size = new System.Drawing.Size(216, 42);
             this.button9.TabIndex = 8;
             this.button9.Text = "button9";
             this.button9.UseVisualStyleBackColor = true;
             // 
             // button10
             // 
-            this.button10.Location = new System.Drawing.Point(5, 507);
+            this.button10.Location = new System.Drawing.Point(4, 422);
+            this.button10.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(270, 50);
+            this.button10.Size = new System.Drawing.Size(216, 42);
             this.button10.TabIndex = 9;
             this.button10.Text = "button10";
             this.button10.UseVisualStyleBackColor = true;
             // 
             // button11
             // 
-            this.button11.Location = new System.Drawing.Point(5, 1);
+            this.button11.Location = new System.Drawing.Point(4, 1);
+            this.button11.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button11.Name = "button11";
-            this.button11.Size = new System.Drawing.Size(90, 65);
+            this.button11.Size = new System.Drawing.Size(72, 54);
             this.button11.TabIndex = 10;
             this.button11.Text = "button11";
             this.button11.UseVisualStyleBackColor = true;
@@ -146,9 +157,10 @@ namespace kopipe_kun
             // 
             // button12
             // 
-            this.button12.Location = new System.Drawing.Point(95, 1);
+            this.button12.Location = new System.Drawing.Point(76, 1);
+            this.button12.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button12.Name = "button12";
-            this.button12.Size = new System.Drawing.Size(90, 65);
+            this.button12.Size = new System.Drawing.Size(72, 54);
             this.button12.TabIndex = 11;
             this.button12.Text = "button12";
             this.button12.UseVisualStyleBackColor = true;
@@ -156,9 +168,10 @@ namespace kopipe_kun
             // 
             // button13
             // 
-            this.button13.Location = new System.Drawing.Point(185, 1);
+            this.button13.Location = new System.Drawing.Point(148, 1);
+            this.button13.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button13.Name = "button13";
-            this.button13.Size = new System.Drawing.Size(90, 65);
+            this.button13.Size = new System.Drawing.Size(72, 54);
             this.button13.TabIndex = 12;
             this.button13.Text = "button13";
             this.button13.UseVisualStyleBackColor = true;
@@ -166,9 +179,9 @@ namespace kopipe_kun
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 562);
+            this.ClientSize = new System.Drawing.Size(224, 468);
             this.Controls.Add(this.button13);
             this.Controls.Add(this.button12);
             this.Controls.Add(this.button11);
@@ -182,10 +195,11 @@ namespace kopipe_kun
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
-            this.Text = "TOP";
+            this.Text = "コピペ君";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
