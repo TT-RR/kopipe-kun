9cb993cc118a1ef415687181ffec2b36ac9306e9
Kamechan516 <105830307+Kamechan516@users.noreply.github.com>
2023-02-15 13:59:24 +0900

    コピペ編集画面　完成

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -41,9 +41,10 @@ namespace kopipe_kun
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(75, 31);
+            this.label1.Location = new System.Drawing.Point(35, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(113, 28);
+            this.label1.Size = new System.Drawing.Size(91, 22);
             this.label1.TabIndex = 0;
             this.label1.Text = "タイトル：";
             // 
@@ -51,58 +52,66 @@ namespace kopipe_kun
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label2.Location = new System.Drawing.Point(78, 107);
+            this.label2.Location = new System.Drawing.Point(35, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(85, 28);
+            this.label2.Size = new System.Drawing.Size(68, 22);
             this.label2.TabIndex = 1;
             this.label2.Text = "内容：";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(208, 36);
+            this.textBox1.Location = new System.Drawing.Point(130, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(187, 22);
+            this.textBox1.Size = new System.Drawing.Size(227, 19);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(83, 154);
+            this.textBox2.Location = new System.Drawing.Point(39, 123);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(392, 170);
+            this.textBox2.Size = new System.Drawing.Size(318, 137);
             this.textBox2.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.ForeColor = System.Drawing.Color.Blue;
-            this.button1.Location = new System.Drawing.Point(192, 344);
+            this.button1.Location = new System.Drawing.Point(144, 275);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(60, 49);
+            this.button1.Size = new System.Drawing.Size(45, 39);
             this.button1.TabIndex = 4;
             this.button1.Text = "保存";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(278, 344);
+            this.button2.Location = new System.Drawing.Point(208, 275);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(57, 49);
+            this.button2.Size = new System.Drawing.Size(43, 39);
             this.button2.TabIndex = 5;
             this.button2.Text = "戻る";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(524, 449);
+            this.ClientSize = new System.Drawing.Size(393, 359);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form2";
             this.Text = "コピペ編集";
             this.ResumeLayout(false);
